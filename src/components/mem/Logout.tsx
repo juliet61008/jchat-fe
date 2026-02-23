448ac741f7c070fec9c2d82831325ecb9e9e1a5f
juliet61008 <juliet61008@cj.net>
2026-02-23 10:08:23 +0900

    로그아웃 로딩

--- src/components/mem/Logout.tsx
+++ src/components/mem/Logout.tsx
@@ -1,9 +1,10 @@
-"use client";
+'use client';
 
-import { logoutServerAction } from "@/utils/auth/authUtil";
-import { useQueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import Loading from '@/components/common/Loading';
+import { logoutServerAction } from '@/utils/auth/authUtil';
+import { useQueryClient } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 const Logout = () => {
   const router = useRouter();
@@ -11,31 +12,28 @@ const Logout = () => {
   const queryClient = useQueryClient();
 
   const processing = async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_JCHAT_API_URL}/auth/logout`,
-      {
-        method: "POST",
-        credentials: "include",
-      }
-    );
+    const res = await fetch(`${process.env.NEXT_PUBLIC_JCHAT_API_URL}/auth/logout`, {
+      method: 'POST',
+      credentials: 'include',
+    });
 
     const fetchRes = await res.json();
 
     if (fetchRes.code !== 0) {
-      queryClient.invalidateQueries({ queryKey: ["user"] });
-      queryClient.invalidateQueries({ queryKey: ["isLogin"] });
+      queryClient.invalidateQueries({ queryKey: ['user'] });
+      queryClient.invalidateQueries({ queryKey: ['isLogin'] });
     }
 
     await logoutServerAction();
 
-    if (res != null) router.replace("/");
+    if (res != null) router.replace('/');
   };
 
   useEffect(() => {
     processing();
   }, []);
 
-  return <div></div>;
+  return <Loading isLoading={true} />;
 };
 
 export default Logout;
